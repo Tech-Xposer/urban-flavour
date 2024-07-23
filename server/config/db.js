0e7d1d9e13708bce400d133d4aa72ceab3f1db15
Ashutosh Sharma <asharma89505@gmail.com>
2024-07-23 15:47:05 +0530

    add database configuration file

---
+++ server/config/db.js
@@ -0,0 +1,12 @@
+
+import mongoose from "mongoose";
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(`${process.env.MONGO_URI}/gourmetgo`);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(`Connection Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+export default connectDB;
