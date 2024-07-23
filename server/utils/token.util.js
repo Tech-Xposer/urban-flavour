2fed079e544b91eaddcdaa8233472db9b94e08c6
Ashutosh Sharma <asharma89505@gmail.com>
2024-07-23 22:15:42 +0530

    feat: add decode-token and generate-token utility

---
+++ server/utils/token.util.js
@@ -0,0 +1,16 @@
+
+import jwt from "jsonwebtoken";
+const SECRET_KEY = process.env.SECRET_TOKEN;
+
+export const generateToken = (payload, expiresIn) => {
+  try {
+    const token = jwt.sign(payload, SECRET_KEY, { expiresIn: expiresIn });
+    return token;
+  } catch (error) {
+    return error.message;
+  }
+};
+
+export const decodeToken = (token) => {
+  return jwt.verify(token, SECRET_KEY);
+};
