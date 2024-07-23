8c7b4bf4e0bffe3abb9033a31f216f8d8dce23e8
Ashutosh Sharma <asharma89505@gmail.com>
2024-07-23 23:28:17 +0530

    feat(routes): add authentication routes for user signup and verification

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { signupUser } from "../controllers/auth.controller.js";
+import { signupUser, verifyUser } from "../controllers/auth.controller.js";
 
 const authRoute = express.Router();
 
 authRoute.post("/signup", signupUser);
+authRoute.get("/verify/:verificationToken", verifyUser);
 
 export default authRoute;
