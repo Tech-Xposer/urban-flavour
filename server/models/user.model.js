fc659dbbf3b8c0487a7e68ef7a50165bcc52a923
Ashutosh Sharma <asharma89505@gmail.com>
2024-07-23 16:10:53 +0530

    Add new User model with basic schema and methods for authentication

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -40,6 +40,58 @@ const userSchema = new mongoose.Schema(
 	}
 );
 
+userSchema.pre("save", async function (next) {
+	if (!this.isModified("password")) {
+		return next();
+	}
+	const salt = await bcrypt.genSalt(10);
+	this.password = await bcrypt.hash(this.password, salt);
+	next();
+});
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+	return await bcrypt.compare(enteredPassword, this.password);
+};
+
+userSchema.methods.generateEmailVerificationToken = function () {
+	const payload = { id: this._id };
+	const verificationToken = jwt.sign(payload, process.env.JWT_SECRET, {
+		expiresIn: "30m",
+	});
+	return verificationToken;
+};
+
+userSchema.methods.generateAccessToken = function () {
+	const payload = { id: this._id };
+	return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
+		expiresIn: "1d",
+	});
+};
+
+userSchema.methods.generateRefreshToken = async function () {
+	const payload = { id: this._id };
+	const refreshToken = jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, {
+		expiresIn: "7d",
+	});
+	this.refreshToken = refreshToken;
+	await this.save(); 
+	return refreshToken;
+};
+
+userSchema.methods.generateRefreshTokenandAccessToken = async function () {
+	const refreshToken = await this.generateRefreshToken(); 
+	const accessToken = this.generateAccessToken();
+	return { accessToken, refreshToken };
+};
+
+userSchema.methods.generatePasswordResetToken = function () {
+	const payload = { id: this._id };
+	const token = jwt.sign(payload, process.env.JWT_SECRET, {
+		expiresIn: "30m",
+	});
+	return token;
+};
+
 const User = mongoose.model("User", userSchema);
 
 export default User;
