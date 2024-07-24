ff23b20257e9d3c0fa3a85d8765002bcb4bb87ac
Ashutosh Sharma <asharma89505@gmail.com>
2024-07-24 18:09:44 +0530

    feat(auth): add currentUser endpoint to retrieve current user's data

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,9 +1,19 @@
 import express from "express";
-import { signupUser, verifyUser, loginUser } from "../controllers/auth.controller.js";
+import {
+  signupUser,
+  verifyUser,
+  loginUser,
+  userLogout,
+  refreshAccessToken,
+} from "../controllers/auth.controller.js";
+import { currentUser } from "../index.js";
 
 const authRoute = express.Router();
 
 authRoute.post("/signup", signupUser);
 authRoute.get("/verify/:verificationToken", verifyUser);
-authRoute.post('/login', loginUser)
+authRoute.post("/login", loginUser);
+authRoute.post("/logout", userLogout);
+authRoute.post("/refresh-token", refreshAccessToken);
+authRoute.get("/current-user", currentUser);
 export default authRoute;
