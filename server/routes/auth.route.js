5d01f21a5fb7c6d403144d1bad49cb1632960dc8
Ashutosh Sharma <asharma89505@gmail.com>
2024-07-25 12:31:39 +0530

    Add authentication routes and secure endpoints with auth middleware

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -5,15 +5,18 @@ import {
   loginUser,
   userLogout,
   refreshAccessToken,
+  currentUser,
 } from "../controllers/auth.controller.js";
-import { currentUser } from "../index.js";
+import { authToLoginUser } from "../middlewares/auth.middleware.js";
 
 const authRoute = express.Router();
 
 authRoute.post("/signup", signupUser);
 authRoute.get("/verify/:verificationToken", verifyUser);
 authRoute.post("/login", loginUser);
-authRoute.post("/logout", userLogout);
-authRoute.post("/refresh-token", refreshAccessToken);
-authRoute.get("/current-user", currentUser);
+
+//Secured Routes
+authRoute.post("/logout",authToLoginUser, userLogout);
+authRoute.post("/refresh-token",authToLoginUser, refreshAccessToken);
+authRoute.get("/current-user", authToLoginUser, currentUser);
 export default authRoute;
