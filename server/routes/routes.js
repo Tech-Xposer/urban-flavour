f3f694453b8364ae36cc960c7f0bb0145a10e477
Ashutosh Sharma <asharma89505@gmail.com>
2024-07-27 16:01:20 +0530

    Integrate routes for recipes, categories, and ratings
    - Set up routes in the main router:
      - /recipes: Connects to recipeRoute
      - /category: Connects to categoryRoute
      - /rating: Connects to ratingRoute

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,7 +1,13 @@
 import express from "express";
 import authRoute from "./auth.route.js";
+import recipeRoute from "./recipe.route.js";
+import categoryRoute from "./category.route.js";
+import ratingRoute from "./rating.route.js";
 
 const router = express.Router();
-router.use('/auth',authRoute)
+router.use("/auth", authRoute);
+router.use("/recipes", recipeRoute);
+router.use("/category", categoryRoute);
+router.use("/rating", ratingRoute);
 
 export default router;
